9592a9d3524d93eb10d058686ba3d354fcd234aa
Maurício Porfírio <mauriciosporfirio@gmail.com>
2018-04-09 15:04:46 -0300

    Ajustando classe de persistência de dados.

--- src/br/estacio/prii/dicionario/persistencia/ArquivoDicionario.java
+++ src/br/estacio/prii/dicionario/persistencia/ArquivoDicionario.java
@@ -1,60 +1,60 @@
 package br.estacio.prii.dicionario.persistencia;
 
-import br.estacio.prii.dicionario.entidade.Palavra;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 
+import java.util.ArrayList;
+import br.estacio.prii.dicionario.entidade.Palavra;
 
 public class ArquivoDicionario 
 {
-     public void gravar(ArrayList<Palavra> palavras)
+    public void gravar(ArrayList<Palavra> palavras)
     {
         try{
-            BufferedWriter arq = new BufferedWriter(new FileWriter(new File("palavras.txt")));
-            StringBuilder str = new StringBuilder();
-            
-            for(Palavra word : palavras){
-                str.setLength(0);
-                str.append(word.getPortugues());
-                str.append(" - ");
-                str.append(word.getIngles());
-                arq.write(str.toString());
-                arq.newLine();
+            try (BufferedWriter arquivo = new BufferedWriter(new FileWriter(new File("dicionario.txt")))) {
+                StringBuilder str = new StringBuilder();
+                
+                for(Palavra p : palavras){
+                    str.setLength(0);
+                    str.append(p.getPortugues());
+                    str.append(" - ");
+                    str.append(p.getIngles());
+                    arquivo.write(str.toString());
+                    arquivo.newLine();
+                }
             }
             
-            arq.close();
-            
         }catch(IOException e){
+            //
         }
     }
     
-    public ArrayList ler()
+    public ArrayList<Palavra> ler()
     {
         ArrayList<Palavra> palavras = new ArrayList<>();
+        
         try{
-            BufferedReader arq = new BufferedReader(new FileReader(new File("palavras.txt")));
-            String palavra[], str;
-            
-            
-            str = arq.readLine();
-            
-            while(str != null){
-                palavra = str.split(" - ");
-                palavras.add(new Palavra(palavra[0], palavra[1]));
-                str = arq.readLine();
+            try (BufferedReader arquivo = new BufferedReader(new FileReader(new File("dicionario.txt")))) {
+                String palavra[], str;
+                str = arquivo.readLine();
+                
+                while(str != null){
+                    palavra = str.split(" - ");
+                    palavras.add(new Palavra(palavra[0], palavra[1]));
+                    str = arquivo.readLine();
+                }
             }
             
-            arq.close();
-            
         }catch(IOException e){
+            //
         }
         
         return palavras;
-        
     }
+    
 }
